a19a3b7ecf92a0e3a55142b89ef72ad5313b2489
Arthur <gaartur4@gmail.com>
2018-06-22 08:41:05 +0300

    added Department class and sources files

--- src/main/java/org/customws/restfulcrud/dao/EmployeeDAO.java
+++ src/main/java/org/customws/restfulcrud/dao/EmployeeDAO.java
@@ -1,16 +1,23 @@
 package org.customws.restfulcrud.dao;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import org.customws.restfulcrud.model.Computer;
 import org.customws.restfulcrud.model.Department;
 import org.customws.restfulcrud.model.Employee;
 
 public class EmployeeDAO {
+
+//    public static final EmployeeDAO DAO_INSTANCE = new EmployeeDAO();
 
     private static final Map<String, Employee> empMap = new HashMap<String, Employee>();
 
@@ -18,7 +25,29 @@ public class EmployeeDAO {
         initEmps();
     }
 
+    private EmployeeDAO() {}
+
+    private File getFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("source.json").getFile());
+        return file;
+    }
+
     private static void initEmps() {
+
+       /* try {
+
+            DAO_INSTANCE.employeeList = new Gson().fromJson(new JsonReader(
+                    new FileReader(DAO_INSTANCE.getFile())), EmployeeDAO.class);
+
+            for (Employee empl : DAO_INSTANCE.employeeList) {
+                empMap.put(empl.getEmployeeID(), empl);
+            }
+        }
+        catch(IOException ioe) {
+            ioe.getStackTrace();
+        }*/
+
         Employee emp1 = new Employee("E_0001", "Tijuana",	"Mccann", "HR Manager");
         Employee emp2 = new Employee("E_0002", "Marg", "Rousseau", "Salesman");
         Employee emp3 = new Employee("E_0003", "Carlos", "Clay", "Auditor");
@@ -37,7 +66,7 @@ public class EmployeeDAO {
 
     public static List<Employee> addEmployee(List<Employee> employees) {
         for (Employee emp: employees
-             ) {
+                ) {
             empMap.put(emp.getEmployeeID(), emp);
         }
         return getAllEmployees();
--- src/main/java/org/customws/restfulcrud/model/Department.java
+++ src/main/java/org/customws/restfulcrud/model/Department.java
@@ -1,5 +1,11 @@
 package org.customws.restfulcrud.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "department")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Department {
 
     private String departmentCode;
